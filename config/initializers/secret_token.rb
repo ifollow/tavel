b641504f847c46836140caf2b8ad230db0a0934b
iFollow Agência <renatoauto@ifollowagencia.com.br>
2014-06-04 22:46:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tavel::Application.config.secret_key_base = '915b0170bf3cdc41e783344543d4646b5c20aae21f344c0d1c15831661261934e188c179e4ee90311fe814203c7d606a0e59dff586b9d72f8a830e9589ec8807'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tavel::Application.config.session_store :cookie_store, key: '_tavel_session'
